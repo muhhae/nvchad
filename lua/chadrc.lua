1595f6e624ade75625dbb752ff5925fa1f94321f
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2026-05-22 15:50:27 +0700

    feat: added some core plugins and keymaps

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,15 +6,15 @@
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+	theme = "catppuccin",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
 }
 
--- M.nvdash = { load_on_startup = true }
+M.nvdash = { load_on_startup = true }
 -- M.ui = {
 --       tabufline = {
 --          lazyload = false
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,17 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map({ "n", "v" }, "d", '"_d')
+map("n", "dd", '"_dd')
+
+map({ "n", "i", "v" }, "<C-x>", "<cmd> wqa <cr>")
+
+map("n", "<C-f>", [[:%s~~~gI<left><Left><Left><Left>]])
+map("x", "<C-f>", [[:s~~~gI<left><Left><Left><Left>]])
+
+map("n", "vw", "viw", { desc = "Select current word" })
+map("n", "vW", "viW", { desc = "Select current WORD" })
+map("n", "vs", "viws", { desc = "Select current word and replace" })
+
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
