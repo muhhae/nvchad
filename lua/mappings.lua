1c717e873772b172ea9b106cad3be650f6e24479
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2026-05-23 09:34:15 +0700

    feat: update mapping and opts

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -12,12 +12,17 @@ map("n", "dd", '"_dd')
 
 map({ "n", "i", "v" }, "<C-x>", "<cmd> wqa <cr>")
 
-map("n", "<C-f>", [[:%s~~~gI<left><Left><Left><Left>]])
-map("x", "<C-f>", [[:s~~~gI<left><Left><Left><Left>]])
+map("n", "<space>rr", [[:%s~~~gI<left><Left><Left><Left>]])
+map("x", "<space>rr", [[:s~~~gI<left><Left><Left><Left>]])
+map(
+  "n",
+  "<leader><leader>",
+  "<cmd>Telescope find_files<cr>",
+  { desc = "telescope find files" }
+)
 
 map("n", "vw", "viw", { desc = "Select current word" })
 map("n", "vW", "viW", { desc = "Select current WORD" })
 map("n", "vs", "viws", { desc = "Select current word and replace" })
 
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> wa <cr>")
